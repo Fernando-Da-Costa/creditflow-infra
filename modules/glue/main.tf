8d33c467cb49dc3521f4446c80748eedd642439c
Fernando Amaral <wellingthon15@hotmail.com>
2025-02-15 14:27:00 -0300

    Criando o job e o cloudwatch

--- modules/glue/main.tf
+++ modules/glue/main.tf
@@ -31,6 +31,12 @@ resource "aws_glue_job" "creditflow_etl" {
   default_arguments = {
     "--job-language"     = "python"
     "--TempDir"         = "s3://${var.s3_bucket}/temp/"
+    "--enable-metrics"                 = "true"   # Ativa métricas do job
+    "--enable-continuous-log-filter"   = "true"   # Ativa filtro contínuo de logs
+    "--continuous-log-logGroup"        = "/aws-glue/jobs/output"
+    "--enable-cloudwatch-log"          = "true"   # Ativa logs no CloudWatch
+    "--job-bookmark-option"            = "job-bookmark-enable" # Mantém o estado do job
+    "--enable-glue-datacatalog"        = "true"   # Ativa o Glue Data Catalog
   }
 }
 
@@ -41,7 +47,9 @@ resource "aws_iam_role" "glue_role" {
 
 data "aws_iam_policy_document" "glue_assume_role" {
   statement {
-    actions = ["sts:AssumeRole"]
+    actions = [
+      "sts:AssumeRole",
+    ]
     principals {
       type        = "Service"
       identifiers = ["glue.amazonaws.com"]
@@ -52,4 +60,22 @@ data "aws_iam_policy_document" "glue_assume_role" {
 resource "aws_iam_role_policy_attachment" "glue_s3_access" {
   role       = aws_iam_role.glue_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+data "aws_iam_policy_document" "glue_policy" {
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "cloudwatch:PutMetricData"
+    ]
+    resources = ["arn:aws:logs:*:*:*", "arn:aws:cloudwatch:*:*:*"]
+  }
+}
+
+resource "aws_iam_role_policy" "glue_policy_attachment" {
+  name   = "GlueCloudWatchPolicy"
+  role   = aws_iam_role.glue_role.id
+  policy = data.aws_iam_policy_document.glue_policy.json
 }
